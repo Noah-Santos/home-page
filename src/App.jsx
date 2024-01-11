25d90999cf82ad53150619ed0976e4e112bd2981
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-01-10 17:47:48 -0700

    Basics
    Have the basic files and tested some images to see how high quality they are

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import galaxy from './galaxy.png';
+import galaxy2 from './DALL·E 2024-01-10 16.41.48 - space with stars and galaxies as a good background.png';
+
+function App() {
+  return (
+    <div>
+      <img src={galaxy} alt="" className='img'/>
+      <img src={galaxy2} alt="" className='img'/>
+    </div>
+  );
+}
+
+export default App;
