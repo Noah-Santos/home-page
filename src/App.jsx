991db774daa99fee97ee6795e09190864148d6f3
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-01-11 16:47:32 -0700

    Background _ Search
    Got the background image to scale appropriately depending on the screen width. Also got the search bar implemented into the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import galaxy from './galaxy.png';
-import galaxy2 from './DALL·E 2024-01-10 16.41.48 - space with stars and galaxies as a good background.png';
+import galaxy from './images/galaxy-transformed.png';
+import earth from './images/earth.JPG'
+import space from './images/space.png';
+import galaxy2 from './images/DALL·E 2024-01-10 16.41.48 - space with stars and galaxies as a good background.png';
+import SearchForm from './Components/SearchBar';
 
 function App() {
   return (
-    <div>
-      <img src={galaxy} alt="" className='img'/>
-      <img src={galaxy2} alt="" className='img'/>
-    </div>
+    <>
+      <div className='backgroundImageContainer'>
+        <img src={earth} alt="" className='img'/>
+        <img src={galaxy} alt="" className='img'/>
+        <img src={space} alt="" className='img'/>
+        <img src={galaxy2} alt="" className='img'/>
+      </div>
+      <SearchForm></SearchForm>
+    </>
   );
 }
 
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,31 @@
+// window.open(`http://www.google.com/search?q=${search}`, '_blank')
+
+import {useRef} from 'react';
+import { IoMdSearch } from "react-icons/io";
+import { CgSearch } from "react-icons/cg";
+
+const SearchBar = () => {
+    let input = useRef('');
+    
+    const search = (e)=>{
+        // prevents the page from reloading when the form is submitted
+        e.preventDefault();
+        // opens the search request in a new google tab
+        window.open(`http://www.google.com/search?q=${input.current.value}`, '_blank')
+    }
+    return (
+        <>
+            <form onSubmit={search} className='searchForm'>
+                <div className="searchContainer">
+                    <input type="text" ref={input} placeholder='Search Google Minus' className='search' autoComplete="off"/>
+                    <IoMdSearch className='searchIcon'></IoMdSearch>
+                    {/* <CgSearch className="searchIcon"></CgSearch> */}
+                </div>
+                {/* ties the input tag to the useRef variable */}
+                {/* <button type='submit'><IoMdSearch /></button> */}
+            </form>
+        </>
+    )
+}
+
+export default SearchBar;
