7b350d61b6fe964458c639c4dc7982e8d09fab58
Noah-Santos <noahbsantos@gmail.com>
2024-01-11 21:05:35 -0700

    Music
    Got the spotify music player to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import galaxy from './images/galaxy-transformed.png';
 import earth from './images/earth.JPG'
+import earth2 from './images/earth2.png'
 import space from './images/space.png';
 import galaxy2 from './images/DALL·E 2024-01-10 16.41.48 - space with stars and galaxies as a good background.png';
 import SearchForm from './Components/SearchBar';
+import Music from './Components/Music';
 
 function App() {
   return (
-    <>
+    <div>
       <div className='backgroundImageContainer'>
+        <img src={earth2} alt="" className='img'/>
         <img src={earth} alt="" className='img'/>
-        <img src={galaxy} alt="" className='img'/>
+        {/* <img src={galaxy} alt="" className='img'/>
         <img src={space} alt="" className='img'/>
-        <img src={galaxy2} alt="" className='img'/>
+        <img src={galaxy2} alt="" className='img'/> */}
       </div>
+      <Music className='musicPlayer'></Music>
       <SearchForm></SearchForm>
-    </>
+    </div>
   );
 }
 
---
+++ src/Components/Music.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Music = () => {
+  return (
+    <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/6eN7LDEdKAEjQGALT3Xj7m?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" className='musicPlayer'></iframe>
+  )
+}
+
+export default Music;
