c08900233b48640526e9beb370dfc92b18e2155b
Noah-Santos <noahbsantos@gmail.com>
2024-01-15 11:25:06 -0700

    Styling
    Did some basic styling for the accordion

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,14 +1,20 @@
 import {useState} from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { CiYoutube, CiInstagram  } from "react-icons/ci";
+import { PiTiktokLogoLight, PiRedditLogo  } from "react-icons/pi";
+import { ImPinterest2 } from "react-icons/im";
+import { FaSpotify } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io5";
+import { SiApplemusic, SiYoutubemusic } from "react-icons/si";
 
 const Accordion = ({name, link}) => {
     const [showInfo, setShowInfo] = useState(false);
     return (
         <article className="accordionSection">
             <header>
-                <h4>{name}</h4>
+                <h3>{name}</h3>
                 <button className='btn' onClick={()=>setShowInfo(!showInfo)}>
-                    {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+                    {showInfo ? <AiOutlineMinus className='accordionBtn'/> : <AiOutlinePlus className='accordionBtn'/>}
                 </button>
             </header>
             <div className="linksCont">
---
+++ src/data.js
@@ -0,0 +1,30 @@
+const Links = [{
+    name: 'Media',
+    link: [
+        ['https://www.youtube.com/', 'Youtube'], 
+        ['https://www.instagram.com/', 'Instagram'], 
+        ['https://www.tiktok.com/en/', 'TikTok'], 
+        ['https://www.pinterest.com/', 'Pinterest'], 
+        ['https://www.reddit.com/', 'Reddit']]
+  },
+  {
+    name: 'School',
+    link: [
+        ['https://west-mec.instructure.com/', 'Canvas West-MEC'], 
+        // ['https://dvusd.instructure.com/calendar#view_name=month&view_start=2024-01-14', 'Canvas DVUSD'], 
+        ['https://focus.west-mec.edu/focus/Modules.php?modname=misc%2FPortal.php', 'Focus'], 
+        // ['https://ps.dvusd.org/guardian/home.html', 'Powerschools'], 
+        ['https://github.com/', 'Github'], 
+        ['https://developer.mozilla.org/en-US/', 'MDN'], ['https://stackoverflow.com/', 'Stackoverflow'], 
+        ['https://www.w3schools.com/', 'w3schools']]
+  },
+  {
+    name: 'Music',
+    link: [
+        ['https://open.spotify.com/', 'Spotify'], 
+        ['https://music.apple.com/us/browse', 'Apple Music'], 
+        ['https://music.youtube.com/', 'Youtube Music'], 
+        ['https://music.amazon.com/', 'Amazon Music']]
+}];
+
+export default Links;
