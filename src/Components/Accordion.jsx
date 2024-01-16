4ad779da38180bbc309e9f91c7a9ee6443f89a01
Noah-Santos <noahbsantos@gmail.com>
2024-01-15 22:05:27 -0700

    Sound
    Got the sound effect to work

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,21 +1,25 @@
 import {useState} from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
-import { CiYoutube, CiInstagram  } from "react-icons/ci";
-import { PiTiktokLogoLight, PiRedditLogo  } from "react-icons/pi";
-import { ImPinterest2 } from "react-icons/im";
-import { FaSpotify } from "react-icons/fa";
-import { IoLogoGithub } from "react-icons/io5";
-import { SiApplemusic, SiYoutubemusic } from "react-icons/si";
+import useSound from 'use-sound';
+import boopSfx from '../sounds/104825__cgeffex__lego_click2.wav';
 
 const Accordion = ({name, link}) => {
     const [showInfo, setShowInfo] = useState(false);
+
+    const BoopButton = () => {
+        const [play] = useSound(boopSfx);
+        return (
+            <button className='btn' onClick={()=>{setShowInfo(!showInfo); play();}}>
+                {showInfo ? <AiOutlineMinus className='accordionBtn'/> : <AiOutlinePlus className='accordionBtn'/>}
+            </button>
+        )
+    };
+
     return (
         <article className="accordionSection">
             <header>
                 <h3>{name}</h3>
-                <button className='btn' onClick={()=>setShowInfo(!showInfo)}>
-                    {showInfo ? <AiOutlineMinus className='accordionBtn'/> : <AiOutlinePlus className='accordionBtn'/>}
-                </button>
+                <BoopButton></BoopButton>
             </header>
             <div className="linksCont">
                 {showInfo && link.map((links, index)=>{
